1bd1de2126362989274b1987e8107d9efcc1b6bc
Manjunath S (IN64504) <m1064504@mindtree.com>
2021-06-02 23:30:33 +0530

    File changed

--- app.ts
+++ app.ts
@@ -3,33 +3,25 @@ import cors from 'cors';
 import connectionToDB from './connection-to-db';
 import router from './Routes/users-route';
 import dotenv from 'dotenv';
-import Services from 'http';
-import Socket from './config/chatSocket';
 dotenv.config();
 
 const startServer = () => {
   const app = express();
-  const chatServices = new Services.Server(app);
-  const chatSocket = Socket(chatServices);
-  chatServices.listen(5000);
-  console.log('Chat Server is running in port 5000');
 
   app.use(express.json());
 
-  app.set('port', process.env.PORT || 4000);
-
-  let http = require('http').Server(app);
-  let io = require('socket.io')(http, http, {
-    cors: {
-      origin: 'http://localhost:4200',
-      methods: ['GET', 'POST'],
-    },
-  });
   app.use(cors());
+
   connectionToDB()
     .then(() => {
       console.log('Connected to database');
-      http.listen(process.env.PORT, function () {
+
+      // app.on("error", (err: any) => {
+      // 	console.log(`Error Connecting to http://localhost:${process.env.PORT}`);
+      // 	console.log(err.message);
+      // });
+
+      app.listen(process.env.PORT, () => {
         console.log(`Server Running at http://localhost:${process.env.PORT}`);
       });
     })
@@ -37,16 +29,8 @@ const startServer = () => {
       console.log(err.message);
     });
 
-  io.on('connection', function (socket: any) {
-    console.log('a user connected');
-    // whenever we receive a 'message' we log it out
-    socket.on('message', function (message: any) {
-      console.log(message);
-    });
-  });
-
   app.use('/api', router);
-  app.use("/uploads", express.static(__dirname + "/uploads"));
+  app.use('/uploads', express.static(__dirname + '/uploads'));
 };
 
 startServer();
